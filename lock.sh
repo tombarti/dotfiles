3a49572558a55e9be5bd35213e00ebc05315fde5
tombarti <tom.bartissol@gmail.com>
2015-12-19 13:03:40 +0000

    added lock icon

--- lock.sh
+++ lock.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-icon="$HOME/.xlock/icon.png"
+icon="$HOME/.i3/lock_icon.png"
 tmpbg='/tmp/screen.png'
 
 (( $# )) && { icon=$1; }
@@ -9,4 +9,4 @@ scrot "$tmpbg"
 convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
 convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
 playerctl play-pause
-i3lock -u -i "$tmpbg"
+i3lock -u -n -i "$tmpbg"
