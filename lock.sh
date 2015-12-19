0e2c783d7cae9f264f2b745f0c482143382c0fd1
tombarti <tom.bartissol@gmail.com>
2015-12-19 12:44:13 +0000

    playerctl controls adde

--- lock.sh
+++ lock.sh
@@ -8,5 +8,5 @@ tmpbg='/tmp/screen.png'
 scrot "$tmpbg"
 convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
 convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
-dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
+playerctl play-pause
 i3lock -u -i "$tmpbg"
